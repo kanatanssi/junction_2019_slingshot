94b529655de2ca8ba32b276b01e483a4ca942647
Informhunter <informhunter@gmail.com>
2019-11-15 22:20:46 +0200

    Flask Socket IO app initial commit.

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, emit
+
+# initialize Flask
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+            
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('joined')
+def handle_joined(message):
+    join_room('main_room')
+    emit('update', {'msg': 'Somebody joined'}, room='main_room')
+
+
+@socketio.on('update_position')
+def handle_text(position):
+    emit('update', {'msg': f"Position of {position['name']} is {position['position']}"}, room='main_room')
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
